21e452ea39f654d2ee54a0d700f18bbae6240412
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-02 11:20:38 +0800

    in_array() should have priority over Collection::contains().
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Traits/SelectionTrait.php
+++ src/Traits/SelectionTrait.php
@@ -184,16 +184,16 @@ trait SelectionTrait
      */
     protected function getSelectedValue($value, $selected)
     {
-        if ($selected instanceof Collection) {
-            return $selected->contains($value) ? 'selected' : null;
-        } elseif ($selected instanceof Arrayable) {
-            $selected = $selected->toArray();
-        }
+        $selection = $selected instanceof Arrayable ? $selected->toArray() : $selected;
 
-        if (is_array($selected)) {
-            return in_array($value, $selected, true) || in_array((string) $value, $selected, true)
-                        ? 'selected'
-                        : null;
+        if (is_array($selection)) {
+            if (in_array($value, $selection, true) || in_array((string) $value, $selection, true)) {
+                return 'selected';
+            } elseif ($selected instanceof Collection) {
+                return $selected->contains($value) ? 'selected' : null;
+            }
+
+            return null;
         }
 
         if (is_int($value) && is_bool($selected)) {
